7b73760365e6d974d54dcb39fbf097959cddd787
tianxin <935839646@qq.com>
2019-01-22 18:21:02 +0800

    day01

---
+++ demo01/demo01/demo01.js
@@ -0,0 +1,8 @@
+
+var app = angular.module('test',[]);
+			app.controller('myctl',function($scope){
+				$scope.name = "tianxin";
+				$scope.myclick = function(){
+						alert("look look me");
+				}
+			});
